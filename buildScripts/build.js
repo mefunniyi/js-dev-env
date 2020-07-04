3acdba9aaf39d3acc4dd07d8e9821cdbe13eb0db
Efunniyi Mustapha <mefunniyi@yahoo.com>
2020-07-04 03:13:35 +0100

    Production build completed

---
+++ buildScripts/build.js
@@ -0,0 +1,17 @@
+/**eslint-disable no-console */
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import chalk from 'chalk';
+
+process.env.NODE_ENV = 'production';
+
+console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
+
+webpack(webpackConfig).run((err) => {
+    if(err) {
+        console.log(chalk.red(err));
+        return 1;
+    }
+    return 0;
+})
+
---
+++ webpack.config.prod.js
@@ -0,0 +1,52 @@
+import path from 'path';
+import webpack from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Generates an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Creates a seperate bundle of vendor libraries that will be cached separately
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    // Create HTML file that includes reference to bundled JS.
+    new HTMLWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    }),
+
+    // Eluminates duplicate packages
+    new webpack.optimize.DedupePlugin,
+
+    // minify js
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+}
+ 
